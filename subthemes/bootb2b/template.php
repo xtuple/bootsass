02eeaa2b08e6dbd3c98e971daef4e70679e53680
Andrey Mikheychik <a.mikheychik@gmail.com>
2014-11-14 16:07:27 -0500

    BootB2B theme added

---
+++ subthemes/bootb2b/template.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @file keeps theme functions overrides
+ */
+
+/**
+ * Overrides theme_status_messages()
+ *
+ * Bootstrap uses different classes to mark messages (based on alert class).
+ *
+ * @param $vars
+ *
+ * @return string
+ *
+ * @see theme_status_messages()
+ * @see drupal_set_message()
+ */
+function bootb2b_status_messages(&$vars) {
+  $display = $vars['display'];
+  $output = '';
+
+  // Keep these statuses, as they are used for assistive technologies
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+
+  // Mapping of Drupal statuses to Bootstrap.
+  $bootstrap_types = array(
+    'status' => 'success',
+    'error' => 'danger',
+    'warning' => 'warning',
+    'info' => 'info',
+  );
+
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $classes = array('alert');
+    if (isset($bootstrap_types[$type])) {
+      $classes[] = "alert-$bootstrap_types[$type]";
+    }
+    else {
+      $classes[] = "alert-$type";
+    }
+    if (count($messages) > 1 && BOOTSTRAP3_ALERT_GROUP) {
+      if (BOOTSTRAP3_ALERT_BLOCK) {
+        $classes[] = "alert-block";
+      }
+      $class = implode(' ', $classes);
+      $output .= "<div class=\"$class\">\n";
+      if (!empty($status_heading[$type])) {
+        $output .= _bootstrap3_element_invisible($status_heading[$type]);
+      }
+      $output .= _bootstrap3_close_button('alert');
+      if (count($messages) > 1) {
+        $output .= " <ul>\n";
+        foreach ($messages as $message) {
+          $output .= '  <li>' . $message . "</li>\n";
+        }
+        $output .= " </ul>\n";
+      }
+      else {
+        $output .= $messages[0];
+      }
+      $output .= "</div>\n";
+    }
+    else {
+      if (BOOTSTRAP3_ALERT_BLOCK) {
+        $classes[] = "alert-block";
+      }
+      $class = implode(' ', $classes);
+      foreach ($messages as $message) {
+        $output .= "<div class=\"$class\">\n";
+        if (!empty($status_heading[$type])) {
+          $output .= _bootstrap3_element_invisible($status_heading[$type]);
+        }
+        $output .= _bootstrap3_close_button('alert');
+        $output .= $message;
+        $output .= "</div>\n";
+      }
+    }
+  }
+
+  return '<div id="status-messages">' . $output . '</div>';
+}
+
+function bootb2b_preprocess_block_menu(&$variables) {
+  if ($variables['name'] == 'menu_main_menu') {
+    if (!empty($variables['context']) && $variables['context'] == 'header') {
+      if ($variables['content'] == '<div class="container-fluid"></div>') {
+        $variables['hide_empty_block'] = TRUE;
+        $variables['title'] = '';
+        $variables['content'] = '';
+      }
+    }
+  }
+}
